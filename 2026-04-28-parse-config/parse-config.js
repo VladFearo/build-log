8b9752e44a73ac9893147851be0cd1c851a7cf83
Vlad Shevtsov <vladsh123@gmail.com>
2026-04-28 17:08:27 +0300

    Validate required config fields

--- 2026-04-28-parse-config/parse-config.js
+++ 2026-04-28-parse-config/parse-config.js
@@ -1,30 +1,44 @@
 const fs = require("fs");
 
-function readJsonFile(filepath){
-    try {
-        const fileContent = fs.readFileSync(filepath, "utf8");
-        const data = JSON.parse(fileContent);
-        return data;
-    } catch (err) {
-        if(err.code==="ENOENT"){
-            console.error("Error: file not found");
-        } else{console.error("Error: invalid JSON");}
-        
-        return null;
+function isNonEmptyString(value) {
+  return typeof value === "string" && value.trim() !== "";
+}
+
+function validateRequiredFields(data) {
+  if (isNonEmptyString(data.appName) && isNonEmptyString(data.version)) {
+    return true;
+  } else {
+    console.error("Error: invalid config");
+    return false;
+  }
+}
+
+function readJsonFile(filepath) {
+  try {
+    const fileContent = fs.readFileSync(filepath, "utf8");
+    const data = JSON.parse(fileContent);
+    return data;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error("Error: file not found");
+    } else {
+      console.error("Error: invalid JSON");
     }
-    
+
+    return null;
+  }
 }
 
 const args = process.argv.slice(2);
 
-if (args.length === 0 ){
-    console.error("Error: missing config file path")
-} else if(args.length > 1){
-    console.error("Error: too many arguments")
+if (args.length === 0) {
+  console.error("Error: missing config file path");
+} else if (args.length > 1) {
+  console.error("Error: too many arguments");
 } else {
-    const configPath = args[0];
-    const data = readJsonFile(configPath);
-    if (data){
+  const configPath = args[0];
+  const data = readJsonFile(configPath);
+  if (data && validateRequiredFields(data)) {
     console.log(data);
-    }
+  }
 }
